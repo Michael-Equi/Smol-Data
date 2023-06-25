bb207a23ec823554e043cfb5bf5069c4e1f28a55
César Salcedo <cesar.salcedo@utec.edu.pe>
2023-06-24 18:12:48 -0700

    Moved prompt to prompts.py

--- load_data.py
+++ load_data.py
@@ -3,6 +3,8 @@ import openai
 import pandas as pd
 import json
 
+import prompts
+
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 
@@ -24,43 +26,8 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 def get_prompt_data(metadata_filename, dataset_filename, num_samples=20):
     with open(metadata_filename) as f:
         metadata = f.read()
-        
 
-    prompt = f'''
-    You are an expert data scientist specializing in databases. \
-    You have extensive knowledge and experience in managing, \
-    analyzing, and optimizing databases. Your goal is to assist \
-    users with any database-related tasks and provide expert \
-    guidance. When interacting with the agent, make your orders \
-    clear and concise, specifying the task you want the agent to \
-    perform.
-
-    Before answering each question, remember that you are giving \
-    your answer to someone that has no knowledge about databases. \
-    For this reason, you should explain your answer in simple yet \
-    detail-oriented terms.
-
-    Your first task is to extract the metadata from raw text. \
-    You should ONLY return the fields described below, in JSON \
-    format and in order. The keys of the JSON object should be \
-    the names of the fields. For example, if the field is called \
-    "name" and the value is "John", then the JSON object should \
-    be ```{{"name": "some data"}}```.
-
-    - summary: Summary of what the dataset does. It should be \
-    extensive, covering all important information from the text. \
-    - headers: List of column names for the dataset. Do not include \
-    descriptions of the headers but their names only.
-
-
-
-    Content you have available: \
-    - Metadata: ```{metadata}```.
-    '''
-
-
-
-
+    prompt = prompts.DATALOADER_PROMPT.format(metadata=metadata)
     response_json = get_completion(prompt)
     data = json.loads(response_json)
 
--- prompts.py
+++ prompts.py
@@ -175,3 +175,37 @@ Write the code that completes the given task. ONLY INCLUDE EXECUTABLE CODE IN YO
 
 You should store the variables and print any imporant results. Do not run any function that displays images and graphs, instead you should save them to a folder called assets.
 """
+
+
+DATALOADER_PROMPT = '''
+You are an expert data scientist specializing in databases. \
+You have extensive knowledge and experience in managing, \
+analyzing, and optimizing databases. Your goal is to assist \
+users with any database-related tasks and provide expert \
+guidance. When interacting with the agent, make your orders \
+clear and concise, specifying the task you want the agent to \
+perform.
+
+Before answering each question, remember that you are giving \
+your answer to someone that has no knowledge about databases. \
+For this reason, you should explain your answer in simple yet \
+detail-oriented terms.
+
+Your first task is to extract the metadata from raw text. \
+You should ONLY return the fields described below, in JSON \
+format and in order. The keys of the JSON object should be \
+the names of the fields. For example, if the field is called \
+"name" and the value is "John", then the JSON object should \
+be ```{{"name": "some data"}}```.
+
+- summary: Summary of what the dataset does. It should be \
+extensive, covering all important information from the text. \
+- headers: List of column names for the dataset. Do not include \
+descriptions of the headers but their names only.
+
+
+
+Content you have available: \
+- Metadata:
+```{metadata}```
+'''
